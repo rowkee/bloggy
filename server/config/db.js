a93439bac3e72683d81412bbe452844b30ce1db2
rowkee <rick.keenan10@gmail.com>
2024-02-12 12:49:20 +0000

    changing url for mongo again

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     mongoose.set("strictQuery", false);
-    const conn = await mongoose.connect(process.env.MONGO_PRIVATE_URL);
+    const conn = await mongoose.connect(process.env.MONGO_DB_URI);
     console.log(`Database Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
